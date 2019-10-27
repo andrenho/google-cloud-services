c554f1eb27d0589e2fa6b3d5fbff4595d7d8163f
André Wagner <andre.nho@gmail.com>
2019-10-27 13:07:21 -0300

    .

--- 0_security/setup.sh
+++ 0_security/setup.sh
@@ -2,4 +2,9 @@
 
 . ../settings.sh
 
-gcloud iam service-accounts create $SERVICE_ACCOUNT
+gcloud iam service-accounts create $SERVICE_ACCOUNT --display-name $SERVICE_ACCOUNT
+
+SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:$SERVICE_ACCOUNT" --format="value(email)")
+echo E-mail: $SA_EMAIL
+
+gcloud iam service-accounts keys create sa_secret.json --iam-account $SA_EMAIL
