dcf4025340092d25086fa3fc7948906569bfd2bc
André Wagner <andre.nho@gmail.com>
2019-10-28 16:42:18 +0000

    .

--- 0_basic/setup_security.sh
+++ 0_basic/setup_security.sh
@@ -4,6 +4,7 @@
 
 gcloud iam service-accounts create $SERVICE_ACCOUNT --display-name $SERVICE_ACCOUNT
 
-SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:$SERVICE_ACCOUNT" --format="value(email)")
+SA_EMAIL=$(get_service_account)
+echo Service account e-mail: $SA_EMAIL
 
 gcloud iam service-accounts keys create sa_secret.json --iam-account $SA_EMAIL
--- settings.sh
+++ settings.sh
@@ -2,3 +2,8 @@ WEBSITE_BUCKET='website-learn-andrenho'
 STORAGE_BUCKET='storage-learn-andrenho'
 SERVICE_ACCOUNT='service-account-andrenho'
 INSTANCE_NAME='learn-instance'
+
+get_service_account() {
+  local r=$(gcloud iam service-accounts list --filter="displayName:$SERVICE_ACCOUNT" --format="value(email)")
+  echo "$r"
+}
